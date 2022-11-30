0cf3d6e791cc1b3b75154e1a3de0f886b2359b40
Vanhue265 <vanhue2652k2@gmail.com>
2022-11-30 14:22:38 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'hbs')
+app.get('/', (req,res)=>{
+    const n = new Date().toLocaleTimeString()
+    console.log("Home duoc goi luc: " + n)
+    res.render('home', {now:n})
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
+console.log("My app is up")
